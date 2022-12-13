1d9019b37f2bf91db6f50c8cd5c29c4460f93b9a
nereno96 <nereno96@gmail.com>
2022-12-13 09:16:54 -0500

    completed exercise 2

--- JsonPlaceholder/scripts/edit-todo.js
+++ JsonPlaceholder/scripts/edit-todo.js
@@ -3,27 +3,46 @@
 window.onload = init;
 
 function init () {
-    const addBtn = document.getElementById("addBtn");
-    addBtn.onclick = addBtnClicked;
+    const getBtn = document.getElementById("getBtn");
+    getBtn.onclick = getBtnClicked;
+    const updateBtn = document.getElementById("updateBtn");
+    updateBtn.onclick = updateBtnClicked;
 }
 
-function addBtnClicked () {
-    const messageDiv = document.getElementById("messageDiv");
-    let toDoId = document.getElementById("toDoId").value;
-    fetch(`https://jsonplaceholder.typicode.com/todos/${toDoId}`)
+function getBtnClicked () {
+    let toDoId = document.getElementById("toDoId");
+    fetch(`https://jsonplaceholder.typicode.com/todos/${toDoId.value}`)
     .then(result => result.json())
     .then(data => {
-        let userId = document.getElementById("userId").value;
-        userId = data.userId;
-        let title = document.getElementById("title").value;
-        title = data.title;
-        let completed = document.getElementById("completed").value;
-        completed = data.title;
-    let bodyData = {
-        
-    }})
+        let userId = document.getElementById("userId");
+        userId.value = data.userId;
+        let title = document.getElementById("title");
+        title.value = data.title;
+        let completed = document.getElementById("completed");
+        completed.value = data.completed;
+        toDoId.disabled = true;})
     .catch(err => {
         messageDiv.innerHTML = "Unexpected error";
     })
 }
 
+function updateBtnClicked (){
+    const messageDiv = document.getElementById("messageDiv");
+    let toDoId = document.getElementById("toDoId").value
+    let bodyData = {
+        userId: document.getElementById("userId").value,
+        title: document.getElementById("title").value,
+        completed: document.getElementById("completed").value
+    }
+    fetch(`https://jsonplaceholder.typicode.com/todos/${toDoId}`, {
+        method: "PUT",
+        body: bodyData
+    })
+    .then(response => response.json())
+    .then(json => {
+        messageDiv.innerHTML = "Student updated";
+    })
+    .catch(err => {
+        messageDiv.innerHTML = "Unexpected error";
+    });
+}
